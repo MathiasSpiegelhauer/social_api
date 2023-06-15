a6d91d36843765001a71a1d3433cb67ce1480a12
MathiasSpiegelhauer <69956883+MathiasSpiegelhauer@users.noreply.github.com>
2023-06-15 16:46:53 +0200

    Update event_get_own.php

--- events/event_get_own.php
+++ events/event_get_own.php
@@ -6,13 +6,13 @@ $user = check_login($REQ['user_token'],$REQ['session']);
 if (isset($REQ['lat']) && isset($REQ['long'])) {
     $lat = $REQ['lat'];
     $long = $REQ['long'];
-    $events = fetch_all("SELECT id, title, description, address, longitude, latitude, creator_id,
+    $events = fetch_all("SELECT id, title, description, address, starttime, longitude, latitude, creator_id,
     (6371 * acos(cos(radians(?)) * cos(radians(latitude)) * cos(radians(longtitude) - radians(?)) + sin(radians(?)) * sin(radians(latitude)))) AS distance
     FROM events
     HAVING distance < 15
     ORDER BY distance WHERE creator_id = ?", [$lat, $long, $lat,$user['id']]);
 } else {
-    $events = fetch_all("SELECT id,token, title, description, address, longitude, latitude, creator_id FROM events WHERE creator_id = ?", [$user['id']]);
+    $events = fetch_all("SELECT id,token, title, description, address, starttime, longitude, latitude, creator_id FROM events WHERE creator_id = ?", [$user['id']]);
 }
 
 //$events = fetch_all("SELECT id,title, description, address, longitude, latitude, creator_id FROM events",[]);
@@ -22,6 +22,7 @@ foreach ($events as $event) {
         'title' => $event['title'],
         'description' => $event['description'],
         'address' => $event['address'],
+        'starttime' => $event['starttime'],
         'coords' => [
             "latitude" => $event['latitude'],
             "longitude" => $event['longitude'],
